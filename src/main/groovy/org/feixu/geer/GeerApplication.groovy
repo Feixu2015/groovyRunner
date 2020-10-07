b3ea894a782519295665954c15f3cd8212535e96
飞絮 <mailofgod@126.com>
2020-10-07 19:31:28 +0800

    prepare habits propose

--- src/main/groovy/org/feixu/geer/GeerApplication.groovy
+++ src/main/groovy/org/feixu/geer/GeerApplication.groovy
@@ -21,36 +21,49 @@ class GeerApplication implements CommandLineRunner, ExitCodeGenerator {
     @Autowired
     private ReportService reportService
 
+    static final String SPECIAL = '65f0dd45-60cf-45c6-a1f6-327fb547659f'
+
+    private static boolean is4Test(String[] args) {
+        null != args && args.size() > 0 && args[0].equals(SPECIAL)
+    }
+
     static void main(String[] args) {
-        System.exit(SpringApplication.exit(SpringApplication.run(GeerApplication, args)))
+        if (is4Test(args)) {
+            SpringApplication.run(GeerApplication, args)
+        } else {
+            System.exit(SpringApplication.exit(SpringApplication.run(GeerApplication, args)))
+        }
     }
 
     @Override
     void run(String... args) throws Exception {
-        String defaultUserDir = reportPath
-        if (null != args && args.size() < 1) {
-            println """使用方法: java -jar reportSystem.jar [excel名，例如: D:/report/检测数据.xlsx] [报告存放文件路径, 例如：D:/report/]
+        if (!is4Test(args)) {
+            String defaultUserDir = reportPath
+            if (null == args || args.size() < 1) {
+
+                println """使用方法: java -jar reportSystem.jar [excel名，例如: D:/report/检测数据.xlsx] [报告存放文件路径, 例如：D:/report/]
 生成的报告为 xxx_检测报告_20201003.pdf
 默认的报告生成目录: ${System.properties['user.dir']}/reports/
 """
-            throw new RuntimeException("启动参数不正确")
-        }
-        String excelFileName = args[0]
-        File excelFile = new File(excelFileName)
-        if (!excelFile.exists()) {
-            println "检测数据文件（${excelFileName}）不存在！"
-        }
-        String targetPath = args.size() > 1 ? args[1] : defaultUserDir
-        File targetFolder = new File(targetPath)
-        if (!targetFolder.exists()) {
-            try {
-                Files.createDirectories(targetFolder.toPath())
-            } catch(e) {
-                throw e
+                throw new RuntimeException("启动参数不正确")
             }
-        }
+            String excelFileName = args[0]
+            File excelFile = new File(excelFileName)
+            if (!excelFile.exists()) {
+                println "检测数据文件（${excelFileName}）不存在！"
+            }
+            String targetPath = args.size() > 1 ? args[1] : defaultUserDir
+            File targetFolder = new File(targetPath)
+            if (!targetFolder.exists()) {
+                try {
+                    Files.createDirectories(targetFolder.toPath())
+                } catch (e) {
+                    throw e
+                }
+            }
 
-        reportService.createReport(excelFile, targetFolder)
+            reportService.createReport(excelFile, targetFolder)
+        }
     }
 
     @Override
--- src/test/groovy/org/feixu/geer/GeerApplicationTests.groovy
+++ src/test/groovy/org/feixu/geer/GeerApplicationTests.groovy
@@ -1,13 +1,157 @@
 package org.feixu.geer
 
-
+import com.alibaba.fastjson.JSON
+import org.apache.commons.lang.StringUtils
+import org.apache.poi.ss.usermodel.Cell
+import org.apache.poi.ss.usermodel.Row
+import org.apache.poi.ss.usermodel.Sheet
+import org.apache.poi.ss.usermodel.Workbook
+import org.apache.poi.xssf.usermodel.XSSFWorkbookFactory
 import org.junit.jupiter.api.Test
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest(args = [ "/Users/idcos/Downloads/zhoulinxian/20200930/检测数据.xlsx" ])
+import java.nio.charset.StandardCharsets
+
+@SpringBootTest(args = [ GeerApplication.SPECIAL ])
 class GeerApplicationTests {
+    private static Logger log = LoggerFactory.getLogger(GeerApplicationTests.class)
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void readEatingHabitsAndLivingHabits() {
+        def data = readEatingHabitsAndLivingHabitsFromFile(new File('/Users/idcos/Downloads/zhoulinxian/报告基础元素/饮食习惯生活习惯.xlsx'))
+        new File('/Users/idcos/Documents/feixu/codes/geer/src/main/resources/data/eating_habits_and_living_habits.json').withWriter(StandardCharsets.UTF_8.toString()) {
+            it.write(JSON.toJSONString(data, true))
+        }
+    }
+
+    private def readEatingHabitsAndLivingHabitsFromFile(File excel) {
+        def ret = [:]
+        InputStream inp = null
+        try {
+            inp = new FileInputStream(excel)
+            //InputStream inp = new FileInputStream("workbook.xlsx")
+            Workbook wb = XSSFWorkbookFactory.create(inp)
+            Sheet sheet = wb.getSheetAt(0)
+            // read each row
+            sheet.collect().eachWithIndex { row, index ->
+                // read each cell
+                def organName = row.getCell(0, Row.MissingCellPolicy.CREATE_NULL_AS_BLANK).toString()
+                println "First Cell Value:$organName"
+                if (index > 0 && null != organName && StringUtils.isNotBlank(organName)) {
+                    def data = [
+                            eatingHabits: [
+                                    'positive': '',
+                                    'other': ''
+                            ],
+                            livingHabits: [
+                                    'positive': [
+                                            'male': [
+                                                    'smoke': '',
+                                                    'nonSmoke': '',
+                                            ],
+                                            'female': [
+                                                    'smoke': '',
+                                                    'nonSmoke': '',
+                                            ]
+                                    ],
+                                    'other': ''
+                            ]
+                    ]
+                    ret.put(organName, data)
+                    8.times {
+                        Cell cell = row.getCell(it, Row.MissingCellPolicy.CREATE_NULL_AS_BLANK)
+                        println "cell value is:$cell"
+                        def value = cell?.toString()
+                        println value
+
+                        switch (it) {
+                            case 1:
+                                // 饮食习惯——阳性
+                                data.eatingHabits.positive = analyzeData(value)
+                                break
+                            case 2:
+                                // 饮食习惯——警告及以下
+                                data.eatingHabits.other = analyzeData(value)
+                                break
+                            case 3:
+                                // 生活习惯-不吸烟-男
+                                data.livingHabits.positive.male.noneSmoke = analyzeData(value)
+                                break
+                            case 4:
+                                // 生活习惯-不吸烟-女
+                                data.livingHabits.positive.female.noneSmoke = analyzeData(value)
+                                break
+                            case 5:
+                                // 生活习惯-吸烟-男
+                                data.livingHabits.positive.male.smoke = analyzeData(value)
+                                break
+                            case 6:
+                                // 生活习惯-吸烟-女
+                                data.livingHabits.positive.female.smoke = analyzeData(value)
+                                break
+                            case 7:
+                                // 生活习惯（没有警告以上等级）
+                                data.livingHabits.other = analyzeData(value)
+                                break
+                            default:
+                                break
+                        }
+
+
+                    }
+                    println ""
+                }
+            }
+        } catch (e) {
+            println e
+        } finally {
+            if (null != inp) {
+                try {
+                    inp.close()
+                } catch (ex) {
+                    log.error(ex.getMessage())
+                }
+            }
+        }
+        ret
+    }
+
+    private def analyzeData(String value) {
+        List<String> array = []
+        value.split('\n').each{ str ->
+            def line = str.replace('\r', '').trim()
+            if (StringUtils.isNotBlank(line)) {
+                array << line
+            }
+        }
+        def innerData = [
+                'head': '',
+                'common': '',
+                'items': []
+        ]
+        array.eachWithIndex { line, i ->
+            if (0 == i) {
+                innerData.head = line
+            } else if (1 == i) {
+                if (!line.startsWith('●')) {
+                    innerData.common = line
+                } else {
+                    innerData.items << ['title': line]
+                }
+            } else {
+                if (line.startsWith('●')) {
+                    innerData.items << ['title': line]
+                } else {
+                    innerData.items.last().content = line
+                }
+            }
+        }
+        innerData
     }
 }
